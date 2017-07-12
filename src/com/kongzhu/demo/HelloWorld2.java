6689804b1cfd05913e11a624616b8da5dbdb2361
Administrator <Administrator@KZ-201702081157>
2017-07-12 11:25:44 +0800

    增加

---
+++ src/com/kongzhu/demo/HelloWorld2.java
@@ -0,0 +1,14 @@
+package com.kongzhu.demo;
+
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        // 声明一个变量，
+        // 变量类型是java.lang.String，变量名是s，
+        // 变量的值是字符串"Hello World"
+        String s = "Hello World";
+        // 控制台Console打印变量s的值，然后换行
+        System.out.println(s);
+    }
+
+}
